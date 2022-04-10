3727a093b3b758fd028518fbd433baaecb4fb4e1
Amanpreet <abinepal@my.bcit.ca>
2022-04-09 20:17:28 -0700

    created textarea in create-note-area and used CSS to fix formatting

--- src/app.js
+++ src/app.js
@@ -5,3 +5,18 @@ const notes = [
     id: 1 
   }
 ]
+
+const createButton = document.querySelector(".fa-solid.fa-circle-plus")
+
+function createNote() {
+  const textArea = document.querySelector(".create-note-area")
+  
+  const createArea = textArea.insertAdjacentHTML("afterbegin", "<textarea>Notes</textarea>")
+  
+  
+  
+
+  
+}
+
+createButton.addEventListener("click", createNote)
