99b69293551bb53aba724256c6110b2a683d3ced
Amanpreet <abinepal@my.bcit.ca>
2022-04-11 15:54:48 -0700

    added read note functionality and added close button

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const notes = [
 
 const createButton = document.querySelector(".fa-solid.fa-circle-plus")
 const textArea = document.querySelector(".write-note-area")
+const readArea = document.querySelector(".read-note-area")
 
 function createNote() {
   if (noteChecker()) {
@@ -26,7 +27,6 @@ function createNote() {
     cancelButton.addEventListener("click", cancelNote)
 
   }
-  
 }  
 
 function noteChecker() {
@@ -49,14 +49,52 @@ function saveNote() {
     textArea.firstChild.remove()
   }
   const noteList = document.querySelector(".notes-list")
-  listedTitle = `<li>${noteTitle}</li>`
+  listedTitle = `<li id = "${notes.length}">${noteTitle}</li>`
   noteList.insertAdjacentHTML("afterbegin", listedTitle)
   
+
+  /*const readButton = document.querySelector(".notes-list")
+  const noteLink = document.querySelector(".read-note-area")
+  noteLink.insertAdjacentHTML("afterbegin", notes[1])
+  readButton.addEventListener("click", readNote)*/
 }
 
 
+const readButton = document.querySelector(".notes-list")
+const closeBtn = `<button class="close">Close</button>`
 
+function readNote(evt) {
+  const id = evt.target.id
+  const read = (notes[id-1])
+  /*const noteLink = document.querySelector(".read-note-area")*/
+  const textBody = read.noteBody.split('\n')
+  const title = (textBody[0])
+  const newText = textBody.splice(1).join('\n')
+  if (readChecker()) {
+    readArea.insertAdjacentHTML("beforeend", `<h1>${newText}</h1><p>${read.title}</p>`)
+  }
+  
+
+  
+  readArea.insertAdjacentHTML("afterbegin", closeBtn)
+  const closeButton = document.querySelector(".close")
+  closeButton.addEventListener("click", closeRead)
+  
+}
+
+function closeRead() {
+  while (readArea.hasChildNodes()) {
+    readArea.firstChild.remove()
+  }
+}
+
+function readChecker() {
+  return !(document.querySelector(".read-note-area"))
+}
 
 createButton.addEventListener("click", createNote)
+readButton.addEventListener("click", readNote)
+
+
 
 
