cea00e1d444ab50b2a2b8669917dbe9328f56eeb
Amanpreet <abinepal@my.bcit.ca>
2022-04-10 21:27:12 -0700

    made save button add to array and displayed the title in the notes-list area

--- src/app.js
+++ src/app.js
@@ -6,23 +6,26 @@ const notes = [
   }
 ]
 
-const saveBtn = `<button class="save">Save</button>`
 
 
 
 const createButton = document.querySelector(".fa-solid.fa-circle-plus")
-
 const textArea = document.querySelector(".write-note-area")
 
 function createNote() {
 
   textArea.insertAdjacentHTML("afterbegin", "<textarea>Notes</textarea>")
+
+  const saveBtn = `<button class="save">Save</button>`
   textArea.insertAdjacentHTML("afterbegin", saveBtn)
+  const saveButton = document.querySelector(".save")
+  saveButton.addEventListener("click", saveNote)
+
 
   const cancelBtn = `<button class="cancel">Cancel</button>`
   textArea.insertAdjacentHTML("afterbegin", cancelBtn)
   const cancelButton = document.querySelector(".cancel")
-  cancelButton.addEventListener("click", cancelNote, {once: false})
+  cancelButton.addEventListener("click", cancelNote)
  
     
 }  
@@ -34,6 +37,24 @@ function cancelNote() {
   
 }
 
+function saveNote() {
+  const noteText = document.querySelector("textarea")
+  const line = noteText.value.split("\n")
+  const noteTitle = line[0]
+  notes.push({title: noteTitle, noteBody: noteText.value, id: notes.length + 1})
+  console.log(notes)
+  while (textArea.hasChildNodes()) {
+    textArea.firstChild.remove()
+  }
+  const noteList = document.querySelector(".notes-list")
+  listedTitle = `<li>${noteTitle}</li>`
+  noteList.insertAdjacentHTML("afterbegin", listedTitle)
+  
+}
+
+
+
 
 createButton.addEventListener("click", createNote, {once: true})
+
 
