10ee7d6b717edd7333f04458bd07f5f5442f510f
Amanpreet <abinepal@my.bcit.ca>
2022-04-10 20:15:41 -0700

    added cancel and save button and made cancel button removetextarea

--- src/app.js
+++ src/app.js
@@ -6,17 +6,34 @@ const notes = [
   }
 ]
 
+const saveBtn = `<button class="save">Save</button>`
+
+
+
 const createButton = document.querySelector(".fa-solid.fa-circle-plus")
 
-function createNote() {
-  const textArea = document.querySelector(".create-note-area")
-  
-  const createArea = textArea.insertAdjacentHTML("afterbegin", "<textarea>Notes</textarea>")
-  
-  
-  
+const textArea = document.querySelector(".write-note-area")
 
+function createNote() {
+
+  textArea.insertAdjacentHTML("afterbegin", "<textarea>Notes</textarea>")
+  textArea.insertAdjacentHTML("afterbegin", saveBtn)
+
+  const cancelBtn = `<button class="cancel">Cancel</button>`
+  textArea.insertAdjacentHTML("afterbegin", cancelBtn)
+  const cancelButton = document.querySelector(".cancel")
+  cancelButton.addEventListener("click", cancelNote, {once: false})
+ 
+    
+}  
+
+function cancelNote() {
+  while (textArea.hasChildNodes()) {
+    textArea.firstChild.remove()
+  }
   
 }
 
-createButton.addEventListener("click", createNote)
+
+createButton.addEventListener("click", createNote, {once: true})
+
